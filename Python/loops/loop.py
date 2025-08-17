82dc46b9c2df948e9b41d59759d06b8ca6735a6a
sfsd <sdfs>
2025-08-17 03:36:13 -0700

    Learning Loops

---
+++ Python/loops/loop.py
@@ -0,0 +1,27 @@
+# LOOPS 
+
+# i = 5
+# for i in range(0, i):
+#     print("happy Birthday")
+
+
+# i = ["asad",1,2]
+# for i in i:
+#     print(i)
+
+#  task  table print 
+
+# number = int(input("enter a number: "))
+# for i in range(1, 11): 
+#   print(f" {number} * {i} = {number * i}")
+
+#  task:  print start to end table
+
+# start  = int(input("enter a starting numner"))
+# end  = int(input("enter a ending numner"))
+# length = 11
+# for i in range(start,end):
+#     for j in range(1,length):
+#         print(f"{i} * {j} = {start * j}")
+#     length -= 1
+
